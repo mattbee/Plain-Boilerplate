a4b2eced3ff3bc26757c6ddfeff9e268b089d1ad
Matthew Bee <matt@todayishould.com>
2012-09-10 09:00:07 +0100

    Minor html updates

--- index.php
+++ index.php
@@ -33,7 +33,7 @@
 
 </footer>
 
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js"></script>
 <script>window.jQuery || document.write('<script src="/js/libs/jquery-1.7.1.min.js"><\/script>')</script>
 
 <script src="/js/min/script-ck.js"></script>
